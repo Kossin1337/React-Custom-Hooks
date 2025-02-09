73fa23eecbb2b6f135dec4267d77aacd2acc6442
kossin1337 <piotrswobodzinski@gmail.com>
2025-02-09 08:32:56 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+const App = () => {
+    return (
+        <div className="application">
+            <h1>React Custom Hooks Playground</h1>
+        </div>
+    );
+};
+
+export default App;
